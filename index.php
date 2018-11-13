4610d1c357c853df0c4efc83583fff39de271261
EC2 Default User <ec2-user@ip-172-31-33-87.eu-west-3.compute.internal>
2018-11-13 09:44:22 +0000

    second comit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$VALEUR_hote='binodb.cwudsllfo6pa.eu-west-3.rds.amazonaws.com';
+$VALEUR_port='3306';
+$VALEUR_nom_bd='bd01';
+$VALEUR_user='binodb';
+$VALEUR_mot_de_passe='binodb01!';
+$connexion = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+$resultats=$connexion->query("SELECT * FROM utilisateur");
+$resultats->setFetchMode(PDO::FETCH_OBJ);
+while( $resultat = $resultats->fetch() )
+{
+        echo 'user : '.$resultat->nom.'<br>';
+}
+$resultats->closeCursor();
+
+?>
